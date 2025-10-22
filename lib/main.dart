2b24a3f1ca5d4f90b887f77566fb8e3cd0557632
bruh <yotailya1996@gmail.com>
2025-10-22 20:05:50 +0400

    v1

--- lib/main.dart
+++ lib/main.dart
@@ -12,11 +12,8 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'ЛР2 — Список карточек',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
-        useMaterial3: true,
-      ),
+      title: 'ЛР3 — Шабров И.А. ЦПИБу-31',
+      theme: ThemeData(primarySwatch: Colors.blue),
       home: const CardListScreen(),
     );
   }
@@ -25,17 +22,23 @@ class MyApp extends StatelessWidget {
 class CardListScreen extends StatelessWidget {
   const CardListScreen({super.key});
 
+  void _handleLikePressed() {
+    debugPrint(" Нажата кнопка лайка!");
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Шабров И.А. ЦПИБу-31'),
-        backgroundColor: Colors.blue,
+        title: const Text('ЛР3 — Шабров И.А. ЦПИБу-31'),
       ),
       body: ListView.builder(
         itemCount: cards.length,
         itemBuilder: (context, index) {
-          return CardItemWidget(data: cards[index]);
+          return CardItemWidget(
+            data: cards[index],
+            onLikeToggle: _handleLikePressed,
+          );
         },
       ),
     );
