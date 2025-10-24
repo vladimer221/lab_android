64bf939d7557282a4de64ed119bb2da8e55002b1
bruh <yotailya1996@gmail.com>
2025-10-25 00:17:58 +0400

    lab4

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -2,7 +2,7 @@ import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   testWidgets('App starts correctly', (WidgetTester tester) async {
-    await tester.pumpWidget(const Lab());
+    await tester.pumpWidget(const LabApp());
     expect(find.text('ЛР4 — Персонажи Гарри Поттера'), findsOneWidget);
   });
 }
